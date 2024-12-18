74716e8234ba6ca09932842657724469c6a0acf8
otaciliow <otacilio.dml@hotmail.com>
2024-12-18 14:49:10 -0300

    Fix firebase dotenv variables

--- src/services/firebaseconnection.ts
+++ src/services/firebaseconnection.ts
@@ -3,13 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
+
+
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "react-linksapp.firebaseapp.com",
-  projectId: "react-linksapp",
-  storageBucket: "react-linksapp.firebasestorage.app",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
